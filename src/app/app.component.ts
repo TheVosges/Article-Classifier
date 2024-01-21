b84ceaea9a117d5b350dc617eac81829261166d0
253802 <253802@student.pwr.edu.pl>
2024-01-21 14:14:02 +0100

    init2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,47 +8,81 @@ import {MatSlideToggleModule} from '@angular/material/slide-toggle';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'docu';
-  public inputType: string = 'text';
-  public result: string[] = [];
-  booleanSwitchValue: boolean = false;
-  public textInputValue: string = '';
+  notActiveColor = "#3d3d3d";
+  activeColor = "#2ECC71";
 
-  textToPredict: string = '';
+  cat1Color = this.notActiveColor;
+  cat2Color = this.notActiveColor;
+  cat3Color = this.notActiveColor;
+  cat4Color = this.notActiveColor;
+  cat5Color = this.notActiveColor;
+  cat6Color = this.notActiveColor;
+
+  catColorList = [this.cat1Color, this.cat2Color, this.cat3Color, this.cat4Color, this.cat5Color, this.cat6Color]
+
+  file: any;
+  fileContent!: string;
+  booleanSwitchValue: boolean = false;
+  textInputValue: string = '';
   prediction: any;
+
   constructor(private predictionService: PredictionService) { }
 
-  onSubmit(form: any) {
-    // Handle text input submission logic
-    console.log('Text Input Value:', this.textInputValue);
-  }
-
   onSwitchChange() {
-    // Handle logic when the switch changes
     console.log('Boolean Switch Value:', this.booleanSwitchValue);
   }
 
   onFileChange(event: any) {
-    // Handle file input change logic
-    const file = event.target.files[0];
-    console.log('Selected File:', file);
+    this.file = event.target.files[0];
+    this.readFileContent(this.file)
+    console.log('File content:', this.fileContent);
   }
 
-  public toggleInputType(): void {
-    console.log("test")
+  private readFileContent(file: File): void {
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      this.fileContent = e.target.result;
+      console.log('File content1:', this.fileContent);
+    };
+    reader.readAsText(file);
   }
 
   public predictCategory(): void {
-    console.log(this.booleanSwitchValue)
-    if (this.booleanSwitchValue){
-      this.predictionService.predict("test").subscribe(
-      (response) => {
-        console.log('Response:', response);
-      },
-      (error) => {
-        console.error('Error:', error);
-      }
-    );
+    console.log(this.file)
+    if (this.booleanSwitchValue) {
+      this.predictionService.predict(this.fileContent).subscribe(
+        (response) => {
+          console.log('Response:', response);
+          const numberArray: number[] = JSON.parse(response.prediction);
+          for (let i = 0; i < numberArray.length; i++) {
+            if (numberArray[i] == 1) {
+              this.catColorList[i] = this.activeColor;
+            } else {
+              this.catColorList[i] = this.notActiveColor;
+            }
+          }
+        },
+        (error) => {
+          console.error('Error:', error);
+        }
+      );
+    } else {
+      this.predictionService.predict(this.textInputValue).subscribe(
+        (response) => {
+          console.log('Response:', response);
+          const numberArray: number[] = JSON.parse(response.prediction);
+          for (let i = 0; i < numberArray.length; i++) {
+            if (numberArray[i] == 1) {
+              this.catColorList[i] = this.activeColor;
+            } else {
+              this.catColorList[i] = this.notActiveColor;
+            }
+          }
+        },
+        (error) => {
+          console.error('Error:', error);
+        }
+      );
     }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,13 @@ import { FormsModule } from '@angular/forms';
 import { PredictionService } from './prediction.service';
 import { BrowserModule } from '@angular/platform-browser'
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, MatSlideToggleModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, MatSlideToggleModule, FormsModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
